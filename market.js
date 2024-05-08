1664b6ae83904dd1b7cecc3669ebc2fe380e1d06
reshmi <158044443+KucherMaks@users.noreply.github.com>
2024-05-08 12:59:57 +0300

    123-124

--- market.js
+++ market.js
@@ -1,6 +1,17 @@
+// // Перевірка підключеного файлу скриптів
+// console.log('Перевірка підключеного файлу скриптів market.js')
 
-// Перевірка підключеного файлу скриптів
-console.log('Перевірка підключеного файлу скриптів market.js')
+//Визначення масиву товарів
+let itemsArray = [
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12 N',
+    // 'Газонокосарка 430',
+    // 'Електричний тример 130',
+    // 'Електрична газонокосарка 320',
+    // 'Акумуляторний оприскувач 12 E',
+]
 
 // Отримання елементу з ідентифікатором items
 let itemsDiv = document.getElementById("items");
@@ -8,7 +19,7 @@ let itemsDiv = document.getElementById("items");
 // Перевірка існування знайденого блоку
 if (itemsDiv) {
     // Вивід знайденого елементу
-    console.log(itemsDiv)
+    // console.log(itemsDiv)
 
     // // Вивід значення поля знайденого елементу
     // console.log('Поле classList: ', itemsDiv.classList)
@@ -27,10 +38,63 @@ if (itemsDiv) {
     // itemsDiv.innerHTML += '<div class = item></div>'
     // itemsDiv.innerHTML += '<div class = item></div>'
     // itemsDiv.innerHTML += '<div class = item></div>'
-    for (let i = 0; i < 100; i++) {
-        itemsDiv.innerHTML += '<div class = item></div>'
-    }
+
+    // for (let i = 0; i < 100; i++) {
+    //     itemsDiv.innerHTML += '<div class = item></div>'
+    // }
+    //Виведення елементів масиву
+    itemsArray.forEach((item, index) => {
+        // console.log(item)
+        // itemsDiv.innerText += item
+        // itemsDiv.innerHTML += `<div class = "item">${item}</div>`
+        itemsDiv.innerHTML += 
+        `
+        <div class = "item">
+            <h2>Товар №${index}</h2>
+            <p>${item}</p>
+        </div>
+        `
+    })
 } else {
-    // Вивід повідомлення про не знайдений блок
+    //Вивід повідомлення про не знайдений блок
     console.log('Блок товарів не знайдено')
 }
+
+// //Визначення масиву товарів
+// // let itemsArray = ['','','','']
+// let itemsArray = [
+//     'Газонокосарка 43',
+//     'Електричний тример 110',
+//     'Електрична газонокосарка 32',
+//     'Акумуляторний оприскувач 12 N',
+//     // 'Газонокосарка 430',
+//     // 'Електричний тример 130',
+//     // 'Електрична газонокосарка 320',
+//     // 'Акумуляторний оприскувач 12 E',
+// ]
+
+// //Виведення елементів масиву
+// itemsArray.forEach((item) => {
+//     console.log(item)
+// })
+
+// //Виведення елементів відсортованого масиву
+// itemsArray.sort().forEach((item) => {
+//     console.log(index + '-й елемент', item)
+// })
+
+// //Виведення в консоль масиву
+// console.log(itemsArray)
+
+// //Виведення в консоль елементів масиву
+// for (let i=0; i<itemsArray.length; i++) {
+//     console.log(itemsArray[i])
+// }
+
+// //Сортування масиву
+// itemsArray = itemsArray.sort()
+
+// //Виведення в консоль номерів та значень елементів масиву
+// for (let i=0; i<itemsArray.length; i++) {
+//     console.log(i + '-й елемент: ', itemsArray[i])
+// }
