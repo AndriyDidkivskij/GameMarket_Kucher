fc01576fa47332456e4b31230525a5b37eb9f68e
reshmi <158044443+KucherMaks@users.noreply.github.com>
2024-05-15 12:33:28 +0300

    added 12 teachers

--- market.js
+++ market.js
@@ -2,23 +2,122 @@
 // console.log('Перевірка підключеного файлу скриптів market.js')
 
 //Визначення масиву товарів
+// let itemsArray = [
+//     'Газонокосарка 43',
+//     'Електричний тример 110',
+//     'Електрична газонокосарка 32',
+//     'Акумуляторний оприскувач 12 N',
+//     'Газонокосарка 44',
+//     'Електричний тример 111',
+//     'Електрична газонокосарка 33',
+//     'Акумуляторний оприскувач 13 N',
+//     'Газонокосарка 45',
+//     'Електричний тример 112',
+//     'Електрична газонокосарка 34',
+//     'Акумуляторний оприскувач 14 N',
+//     // 'Газонокосарка 430',
+//     // 'Електричний тример 130',
+//     // 'Електрична газонокосарка 320',
+//     // 'Акумуляторний оприскувач 12 E',
+// ]
+
 let itemsArray = [
-    'Газонокосарка 43',
-    'Електричний тример 110',
-    'Електрична газонокосарка 32',
-    'Акумуляторний оприскувач 12 N',
-    'Газонокосарка 44',
-    'Електричний тример 111',
-    'Електрична газонокосарка 33',
-    'Акумуляторний оприскувач 13 N',
-    'Газонокосарка 45',
-    'Електричний тример 112',
-    'Електрична газонокосарка 34',
-    'Акумуляторний оприскувач 14 N',
-    // 'Газонокосарка 430',
-    // 'Електричний тример 130',
-    // 'Електрична газонокосарка 320',
-    // 'Акумуляторний оприскувач 12 E',
+    {
+        firstName: "Віталій",
+        lastName: "Шатківський",
+        age: 43,
+        subject: "CS",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/",
+    },
+    {
+        firstName: "Наталія",
+        lastName: "Венцель",
+        age: 18,
+        subject: "Адміністратор",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/ventsel-nataliya-vasylivna-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/ventsel-n-v/",
+    },
+    {
+        firstName: "Наталія",
+        lastName: "Кучер",
+        age: 18,
+        subject: "Фізика",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/kucher-nataliya-viktorivna-683x1024.jpg", 
+        url: "https://lyceum.ztu.edu.ua/team/kucher-n-v/",
+    },
+    {
+        firstName: "Анжела",
+        lastName: "Лознюк",
+        age: 18,
+        subject: "Українська мова, література",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/10/photo_2023-10-25_15-46-33-1-693x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/loznyuk-anzhela-leonidivna/",
+    },
+    {
+        firstName: "Ірина",
+        lastName: "Боровська-Карандюк",
+        age: 18,
+        subject: "Українська мова, література, зарубіжна література",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/borovska-karandyuk-iryna-anatoliyivna-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/borovska-karandyuk-iryna-anatoliyivna/",
+    },
+    {
+        firstName: "Василь",
+        lastName: "Бабій",
+        age: 18,
+        subject: "Українська мова, література",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/babiy-vasyl-dmytrovych-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/babiy-vasyl-dmytrovych/",
+    },
+    {
+        firstName: "Тарас",
+        lastName: "Савінков",
+        age: 18,
+        subject: "Англійська мова",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/savinkov-taras-yevhenovych-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/savinkov-taras-yevhenovych/",
+    },
+    {
+        firstName: "Вікторія",
+        lastName: "Нелипович",
+        age: 18,
+        subject: "Математика",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/nelypovych-viktoriya-vitaliyivna-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/nelypovych-viktoriya-vitaliyivna/",
+    },
+    {
+        firstName: "Іван",
+        lastName: "Севрук",
+        age: 18,
+        subject: "Захист України",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/sevruk-ivan-hryhorovych-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/sevruk-i-h/",
+    },
+    {
+        firstName: "Олена",
+        lastName: "Василівна",
+        age: 18,
+        subject: "Математика",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/chuhunova-olena-vasylivna-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/chuhunova-o-v/",
+    },
+    {
+        firstName: "Олена",
+        lastName: "Геча",
+        age: 18,
+        subject: "Історія",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/hecha-olena-anatoliyivna-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/hecha-o-a/",
+    },
+    {
+        firstName: "Ірина",
+        lastName: "Глібко",
+        age: 18,
+        subject: "Англійська мова",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/01/hlibko-iryna-anatoliyevna.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/hlibko-iryna-anatoliyivna/",
+    },
 ]
 
 // Отримання елементу з ідентифікатором items
@@ -59,8 +158,11 @@ if (itemsDiv) {
         itemsDiv.innerHTML += 
         `
         <div class = "item">
-            <h2>Товар №${index+1} з ${itemsArray.length}</h2>
-            <p>${item}</p>
+            <h2>Вчитель №${index+1} з ${itemsArray.length}</h2>
+            <p>${item.firstName} ${item.lastName}</p>
+            <p>Предмет: ${item.subject}</p>
+            <p><img src = "${item.photo}" class = "item-image"></p>
+            <p><a href = ${item.url}" target = "_blank" class = "bonus price">Профіль</a></p>
         </div>
         `
     })
